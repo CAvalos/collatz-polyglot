2bc0880d0b33a4e0fd8576fd1b45bd60af12bbb8
CAvalos <cavalos14@smpanthers.org>
2014-05-19 16:34:11 -0700

    Update collatz.js

--- collatz.js
+++ collatz.js
@@ -1,1 +1,17 @@
 console.log("Christian's Collatz");
+var collatz = function(n) {
+    console.log(n);
+    while (n > 1) {
+        if (n % 2 === 0) {
+            n = (n / 2);
+        }
+        else {
+            n = (3 * n + 1);
+        }
+       
+
+    console.log(n);
+    }
+};
+
+return collatz(59);
